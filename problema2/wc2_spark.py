cf4565350117e9fdb267a08c40867217a57a8551
Leonardo Alves dos Santos <leonardo.santos@ccc.ufcg.edu.br>
2016-03-16 21:17:02 -0300

    problem 2, ep 2

--- problema2/wc2_spark.py
+++ problema2/wc2_spark.py
@@ -1,8 +1,19 @@
 def output(text):
     word = text.split(',')
-    return (word[1][12:]+word[2][13:],1)
+    return ("%s|%s"%(word[1][13:-1],word[2][14:-1]),1)
+
+def output0(text):
+    word = text.split(',')
+    return "%s|%s"%(word[1][13:-1],word[2][14:-1])
+
+def output1(text):
+    word = text.split('|')
+    return (word[0],1)
 
 text_file = sc.textFile("input.txt")
 counts = text_file.flatMap(lambda line: line.split("\n"))
 counts = counts.map(lambda word:  output(word))
+counts = counts.reduceByKey(lambda a, b: a + b)
+counts = counts.map(lambda word:  output1(word[0]))
+counts = counts.reduceByKey(lambda a, b: a + b)
 counts.saveAsTextFile("out2")
